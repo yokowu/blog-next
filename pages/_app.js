92d247117638d19f7668f1bd5ec3786380ba087d
Yoko <wuyukang@qzcloud.com>
2021-03-30 10:22:31 +0800

    markdown 渲染

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/index.css'
 import '../styles/post.css'
+import '../styles/hljs.css'
 import 'antd/dist/antd.css'
 
 function MyApp({ Component, pageProps }) {
--- pages/post/index.js
+++ pages/post/index.js
@@ -1,46 +1,34 @@
 import Head from 'next/head'
 import { Col, Row } from 'antd'
-import React from 'react'
-import ReactMarkdown from 'react-markdown'
+import React, { useEffect, useState } from 'react'
 import Footer from '../../components/footer'
 import Header from '../../components/header'
+import marked from 'marked'
+import hljs from "highlight.js"
+import 'highlight.js/styles/nord.css'
 
-let markdown='# P01:课程介绍和环境搭建\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-   '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~这是加删除线的文字~ \n\n'+
-  '```console.log(111)``` \n\n'+
-  '# p02:来个Hello World 初始Vue3.0\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n'+
-  '***\n\n\n' +
-  '# p03:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p04:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '#5 p05:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p06:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p07:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '``` var a=11; ```'
+ const renderer = new marked.Renderer()
+ marked.setOptions({
+  renderer: renderer, 
+  gfm: true,
+  pedantic: false,
+  sanitize: false,
+  tables: true,
+  breaks: false,
+  smartLists: true,
+  smartypants: false,
+  highlight: function (code) {
+    return hljs.highlightAuto(code).value;
+  }
+ })
 
 const Post = () => {
+  const [html, setHtml] = useState('')
+  useEffect(_ => {
+    let h = marked("")
+    setHtml(h)
+  }, [])
+
   return (
     <div className="post">
       <Head>
@@ -57,9 +45,7 @@ const Post = () => {
       <div>
         <Row justify="center">
           <Col xs={16} sm={16} md={16} lg={16} xl={16} className="post-content">
-            <ReactMarkdown
-              children={markdown}
-            />
+            <div dangerouslySetInnerHTML={{ __html: html }} />
           </Col>
         </Row>
       </div>
