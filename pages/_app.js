6ff51bbf3406850e6c1d68040b4d1ec07023bda1
Yoko <wuyukang@qzcloud.com>
2021-04-02 17:55:04 +0800

    增加Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,23 @@ import '../styles/index.css'
 import '../styles/post.css'
 import '../styles/hljs.css'
 import 'antd/dist/antd.css'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react'
+import * as gtag from '../utils/gtag'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
